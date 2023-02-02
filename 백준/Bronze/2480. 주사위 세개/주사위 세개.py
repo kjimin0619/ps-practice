01f80ff8f20ed1259d95538c196710ffdeb0df47
Jimin Kim <74192997+kjimin0619@users.noreply.github.com>
2023-02-02 14:24:45 +0900

    [Bronze IV] Title: 주사위 세개, Time: 64 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.py
@@ -0,0 +1,12 @@
+A,B,C = map(int, input().split())
+
+if (A == B and A == C and B == C) :
+    prize = 10000 + A*1000
+elif (A != B and A != C and B != C) :
+    prize = max([A,B,C])*100
+else :
+    # 2개는 같은 눈
+    if (A == B or A == C) : prize = 1000 + 100*A
+    else : prize = 1000 + 100*B    
+    
+print(prize)
