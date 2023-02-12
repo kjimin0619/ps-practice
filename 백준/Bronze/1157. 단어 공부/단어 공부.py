cff390ce9596d79c3e094a53f45b3ddd007385d5
Jimin Kim <74192997+kjimin0619@users.noreply.github.com>
2023-02-12 13:53:53 +0900

    [Bronze I] Title: 단어 공부, Time: 84 ms, Memory: 33212 KB -BaekjoonHub

--- 백준/Bronze/1157. 단어 공부/단어 공부.py
+++ 백준/Bronze/1157. 단어 공부/단어 공부.py
@@ -1,10 +1,15 @@
 import sys
 
 word = sys.stdin.readline().upper().strip() # 대문자로 변경
-alpha = list(range(65,91)) # A : 65, Z : 90
-cnt = [0]*26
+unique_word = list(set(word)) # 중복값 제거
 
-for w in word: cnt[ord(w)%65]+=1
+cnt = []
+for x in unique_word :
+    cnt.append(word.count(x))
+    
 m = max(cnt)
-if cnt.count(m) > 1 : print("?")
-else : print(chr(alpha[cnt.index(m)]))
+if cnt.count(m) > 1 : 
+    print("?")
+else : 
+    m_index = cnt.index(m)
+    print(unique_word[m_index])
