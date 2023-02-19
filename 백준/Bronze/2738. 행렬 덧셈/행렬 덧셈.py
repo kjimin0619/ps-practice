796314bab78d621dd559e598a1974ce68fda1048
Jimin Kim <74192997+kjimin0619@users.noreply.github.com>
2023-02-19 14:15:09 +0900

    [Bronze V] Title: 행렬 덧셈, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.py
+++ 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.py
@@ -1,12 +1,10 @@
+import sys
+input = sys.stdin.readline
+
 n,m = map(int, input().split())
 
-a1 = []
-for _ in range(n):
-    a1.append(list(map(int, input().split())))
-    
-a2 = []
-for _ in range(n):
-    a2.append(list(map(int, input().split())))
+a1 = [list(map(int, input().split())) for _ in range(n)]
+a2 = [list(map(int, input().split())) for _ in range(n)]
     
 for i in range(n):
     for j in range(m):
