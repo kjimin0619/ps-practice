fed0ce16a9083ffdf39810bca5ab7455c1b4689a
Jimin Kim <74192997+kjimin0619@users.noreply.github.com>
2023-05-05 20:02:25 +0900

    [Silver II] Title: 촌수계산, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/2644. 촌수계산/촌수계산.py
+++ 백준/Silver/2644. 촌수계산/촌수계산.py
@@ -1,9 +1,11 @@
 from sys import stdin as s
-from collections import deque
+#s = open("input.txt","rt")
 n = int(s.readline().strip())
 start, end = map(int,s.readline().split())
 m = int(s.readline().strip())
 relation = [[]*(n+1) for _ in range(n+1)]
+visited = [False]*(n+1)
+res = [] # 탐색 횟수
 
 # 관계 그래프 형성
 for _ in range(m):
@@ -11,22 +13,18 @@ for _ in range(m):
     relation[child].append(parent)
     relation[parent].append(child)
 
-def dfs(start, end):
-    relate_count = [0]*(n+1)
-    visited = [False]*(n+1)
-    stack = deque([start])
-    while stack:
-        now = stack.pop()
-        one_relation = relation[now]
-        for f in one_relation:
-            if visited[f] == False :
-                relate_count[f] = relate_count[now] + 1
-                stack.append(f)
-                visited[f] = True
-    return relate_count
 
-result = dfs(start,end)
-if result[end] == 0 :
-    print(-1)
-else :
-    print(result[end])
+def dfs(start,cnt):
+    visited[start] = True
+    cnt += 1
+
+    if start == end :
+        res.append(cnt)
+        
+    for i in relation[start]:
+        if not visited[i]:
+            dfs(i, cnt)
+
+dfs(start,-1)
+if len(res) == 0 : print(-1)
+else : print(res[0])
