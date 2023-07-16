c5f78f3b6f3a7eaed18447ca969b322cbabad157
Jimin Kim <74192997+kjimin0619@users.noreply.github.com>
2023-07-16 22:58:58 +0900

    [Silver III] Title: Four Squares, Time: 60 ms, Memory: 31688 KB -BaekjoonHub

--- 백준/Silver/17626. Four Squares/Four Squares.py
+++ 백준/Silver/17626. Four Squares/Four Squares.py
@@ -1,19 +1,16 @@
-import math
 n = int(input())
+arr = [0 if i**0.5%1 else 1 for i in range(n+1)] # 제곱수는 1로 저장
 
-# n보다 작거나 같은 제곱수를 찾은 후
-# "n-제곱수"를 인덱스로 가진 값에 1을 더해준다
-
-dp = [0]*(n+1)
-dp[1] = 1
-for i in range(2,n+1):
-    quo = math.floor(math.sqrt(i))
-    remain = i - quo*quo
-    min_value = dp[remain]
-
-    for k in range(1, quo+1):
-        min_value = min(min_value, dp[i - k*k])
-
-    dp[i] = min_value + 1
-
-print(dp[-1])
+min_ = 4
+for i in range(int(n**0.5), 0, -1):
+    if arr[n] : # n이 제곱수일 경우
+        min_=1
+        break
+    elif arr[n-i**2] : # 나머지가 제곱수일 경우
+        min_=2
+        break
+    else:
+        for j in range(int((n-i**2)**0.5), 0, -1):
+            if arr[(n-i**2)-j**2]: # 제곱수를 한번 더 뺀 나머지가 제곱수일 경우
+                min_=3
+print(min_)
