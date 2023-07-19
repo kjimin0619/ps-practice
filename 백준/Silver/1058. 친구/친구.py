462b7439f0c3b8e0d420a2f9c0db1d1ebe7e9708
Jimin Kim <74192997+kjimin0619@users.noreply.github.com>
2023-07-20 04:01:09 +0900

    [Silver II] Title: 친구, Time: 64 ms, Memory: 34144 KB -BaekjoonHub

--- 백준/Silver/1058. 친구/친구.py
+++ 백준/Silver/1058. 친구/친구.py
@@ -1,6 +1,6 @@
 from sys import stdin as s
 from collections import deque
-# s = open("input.txt", "rt")
+
 n = int(s.readline())
 friend2 = []
 graph = [[]]
@@ -11,17 +11,16 @@ for i in range(n):
 
 # bfs - queue
 def bfs(k):
-    frnds = []
     q = deque()
     q.append(k)
     i = 0
     visited = [False] * (n+1)
     visited[k] = True
+    
     for frnd in graph[k] :
-        if (visited[frnd] == False):
-            visited[frnd] = True
-            frnds.append(frnd)
-            q.append(frnd)
+        visited[frnd] = True
+        i+=1
+        q.append(frnd)
 
     while q :
         n_curr = q.popleft()
@@ -29,8 +28,8 @@ def bfs(k):
         for frnd in graph[n_curr] :
             if (visited[frnd] == False):
                 visited[frnd] = True
-                frnds.append(frnd)
-    friend2.append(len(frnds))
+                i+=1
+    friend2.append(i)
             
 for i in range(1,n+1):
     bfs(i)
