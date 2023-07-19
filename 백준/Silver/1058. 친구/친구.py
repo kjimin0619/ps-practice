13d57f44750f899ef110d5782e8e893fbd91c521
Jimin Kim <74192997+kjimin0619@users.noreply.github.com>
2023-07-20 03:56:16 +0900

    [Silver II] Title: 친구, Time: 64 ms, Memory: 34176 KB -BaekjoonHub

---
+++ 백준/Silver/1058. 친구/친구.py
@@ -0,0 +1,38 @@
+from sys import stdin as s
+from collections import deque
+# s = open("input.txt", "rt")
+n = int(s.readline())
+friend2 = []
+graph = [[]]
+
+for i in range(n):
+    f = list(s.readline().strip())
+    graph.append([i+1 for i in range(n) if f[i] == 'Y'])
+
+# bfs - queue
+def bfs(k):
+    frnds = []
+    q = deque()
+    q.append(k)
+    i = 0
+    visited = [False] * (n+1)
+    visited[k] = True
+    for frnd in graph[k] :
+        if (visited[frnd] == False):
+            visited[frnd] = True
+            frnds.append(frnd)
+            q.append(frnd)
+
+    while q :
+        n_curr = q.popleft()
+        visited[n_curr] = True
+        for frnd in graph[n_curr] :
+            if (visited[frnd] == False):
+                visited[frnd] = True
+                frnds.append(frnd)
+    friend2.append(len(frnds))
+            
+for i in range(1,n+1):
+    bfs(i)
+
+print(max(friend2))
