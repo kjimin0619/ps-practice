33175be76eefc89d49549632a5a7d2227be12bf4
Jimin Kim <74192997+kjimin0619@users.noreply.github.com>
2023-07-28 16:08:39 +0900

    [Silver III] Title: 조합, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/2407. 조합/조합.py
+++ 백준/Silver/2407. 조합/조합.py
@@ -5,15 +5,4 @@ def factorial(n):
         return 1
     return n*factorial(n-1)
 
-if n == m :
-    print(1)
-else :
-    # 분모
-    res = 1
-    for i in range(m):
-        res = res * n
-        n = n - 1
-
-    # 분자
-    b = factorial(m)
-    print(res//b)
+print(factorial(n)//(factorial(m)*factorial(n-m)))
