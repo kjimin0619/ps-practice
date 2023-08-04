a138db87925a744b2e78796e6426786ed9185cab
Jimin Kim <74192997+kjimin0619@users.noreply.github.com>
2023-08-04 17:51:51 +0900

    [Gold V] Title: ABCDE, Time: 896 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Gold/13023. ABCDE/ABCDE.py
@@ -0,0 +1,37 @@
+import sys
+from sys import stdin as s
+sys.setrecursionlimit(10**6)
+
+result = 0
+n, m = map(int, s.readline().split())
+network = [[] for _ in range(n)]
+visited = [False] * n
+
+for _ in range(m):
+    a,b = map(int, s.readline().split())
+    network[a].append(b)
+    network[b].append(a)
+
+def dfs(start, cnt):
+    global result
+    visited[start] = True
+    cnt +=1
+    
+    if  cnt == 5:
+        result = 1
+        return 
+    
+    for f in network[start]:
+        if visited[f] == False :
+            visited[f] = True
+            dfs(f, cnt)
+            
+    visited[start] = False
+    
+for i in range(n):
+    if result == 1 :
+        break
+    
+    dfs(i, 0)
+    
+print(result)
