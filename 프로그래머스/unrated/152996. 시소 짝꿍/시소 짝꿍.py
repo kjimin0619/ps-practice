898d0cf4eab97354c9b6c2b6787c822b3ec33f19
Jimin Kim <74192997+kjimin0619@users.noreply.github.com>
2023-08-30 17:20:52 +0900

    [unrated] Title: 시소 짝꿍, Time: 0.04 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/152996. 시소 짝꿍/시소 짝꿍.py
@@ -0,0 +1,11 @@
+from collections import Counter
+
+def solution(weights):
+    answer = 0
+    counter = Counter(weights)
+    for i in counter:
+        answer += counter[i] * (counter[i] - 1) / 2 # 1 : 1
+        answer += counter[i] * counter[i*3/2] # 2 : 3
+        answer += counter[i] * counter[i*2] # 2 : 4
+        answer += counter[i] * counter[i*4/3] # 3 : 4
+    return answer
