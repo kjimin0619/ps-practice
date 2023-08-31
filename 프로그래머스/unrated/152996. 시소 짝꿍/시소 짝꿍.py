b51cf889210fefc03c41deb0079fd611d11779ba
Jimin Kim <74192997+kjimin0619@users.noreply.github.com>
2023-08-31 19:10:10 +0900

    [unrated] Title: 시소 짝꿍, Time: 0.77 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/unrated/152996. 시소 짝꿍/시소 짝꿍.py
+++ 프로그래머스/unrated/152996. 시소 짝꿍/시소 짝꿍.py
@@ -1,11 +1,28 @@
-from collections import Counter
-
 def solution(weights):
-    answer = 0
-    counter = Counter(weights)
-    for i in counter:
-        answer += counter[i] * (counter[i] - 1) / 2 # 1 : 1
-        answer += counter[i] * counter[i*3/2] # 2 : 3
-        answer += counter[i] * counter[i*2] # 2 : 4
-        answer += counter[i] * counter[i*4/3] # 3 : 4
+    dist = [0] * 100001
+    
+    answer= 0
+    weights_kind = set(weights)
+    for w in  weights_kind :
+        cnt = weights.count(w)
+        print(w, cnt)
+        # 1 : 1
+        if w >= 2 :
+            answer += (cnt * (cnt - 1) / 2)
+        
+        # 2 : 3
+        cnt_23 = weights.count(w*(3/2))
+        if cnt_23 > 0 :
+            answer += cnt * cnt_23
+        
+        # 2 : 4
+        cnt_24 = weights.count(w*2)
+        if cnt_24 > 0 :
+            answer += cnt * cnt_24
+            
+        # 3 : 4
+        cnt_34 = weights.count(w*(4/3))
+        if cnt_34 > 0 :
+            answer += cnt * cnt_34
+    
     return answer
