0f3b4a81b9deb34f5517f02109f976dec10236a8
Jimin Kim <74192997+kjimin0619@users.noreply.github.com>
2024-01-26 17:32:15 +0900

    [Bronze V] Title: X보다 작은 수, Time: 48 ms, Memory: 32140 KB -BaekjoonHub

--- 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.py
+++ 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.py
@@ -1,6 +1,7 @@
 import sys
-num, x =  map(int, sys.stdin.readline().split(" "))
-numList = list(map(int, sys.stdin.readline().split(" ")))
 
-for i in numList :
-    if i < x : print(i, end = " ")
+n, x = map(int, sys.stdin.readline().split(" "))
+myList = list(map(int, sys.stdin.readline().split(" ")))
+
+for num in myList:
+  if num < x: print(num, end=" ")
