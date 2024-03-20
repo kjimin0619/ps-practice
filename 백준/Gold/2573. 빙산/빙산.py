1da69e0884f2b75eb976a7c2136052f4f2c4165d
Jimin Kim <74192997+kjimin0619@users.noreply.github.com>
2024-03-20 16:06:04 +0900

    [Gold IV] Title: 빙산, Time: 568 ms, Memory: 151568 KB -BaekjoonHub

--- 백준/Gold/2573. 빙산/빙산.py
+++ 백준/Gold/2573. 빙산/빙산.py
@@ -1,55 +1,56 @@
-from collections import deque
+# 2573 빙산
 import sys
-input = sys.stdin.readline
+from collections import deque
 
-d = [[-1,0],[1,0],[0,1],[0,-1]]
-
-def bfs(y,x):
-    queue = deque()
-    queue.append([y,x])
-    visited[y][x] = True
-
-    while queue:
-        r,c = queue.popleft()
-        for i in range(4):
-            dr = r + d[i][0]
-            dc = c + d[i][1]
-
-            if (0<=dr<n) and (0<=dc<m):
-                if graph[dr][dc] == 0:
-                    visited[r][c] += 1
-
-                if not visited[dr][dc] and graph[dr][dc] != 0 :
-                    queue.append([dr,dc])
-                    visited[dr][dc] = True
+n, m = map(int, sys.stdin.readline().split())
+graph = [list(map(int, sys.stdin.readline().split())) for _ in range(n)]
+time = 0  # 시간의 경과
+direction = [(-1, 0), (1, 0), (0, 1), (0, -1)]  # 이동 방향
 
 
-n , m = map(int,input().split())
-graph = [list(map(int,input().split())) for _ in range(n)]
+# 주변 탐색
+def bfs(x, y):
+  q = deque([(x, y)])
+  visited[x][y] = 1
+
+  while q:
+    cx, cy = q.popleft()  # 현 위치
+    for dx, dy in direction:
+      nx, ny = cx + dx, cy + dy
+      if (0 <= nx < n and 0 <= ny < m):
+        if graph[nx][ny] == 0:  # 주변이 바다
+          visited[cx][cy] += 1  # 현 위치 방문 정보에 바다 개수 저장
+
+        if visited[nx][ny] == 0 and graph[nx][ny] != 0:  # 주변이 빙하
+          q.append((nx, ny))
+          visited[nx][ny] = 1
 
 
-#두 덩어리 이상으로 분리되는 최초의 시간(년)을 구하는 프로그램
-time = 0
 while True:
-    count = 0
-    visited = [[0] * m for _ in range(n)]
-    for i in range(n):
-        for k in range(m):
-            if not visited[i][k] and graph[i][k]!=0:
-                bfs(i,k)
-                count +=1
+  count = 0  # 생성된 빙하 덩어리 수
+  visited = [[0] * m for _ in range(n)]  # 방문 여부
+  # 빙하 덩어리 탐색 후 카운팅
+  for i in range(n):
+    for j in range(m):
+      if visited[i][j] == 0 and graph[i][j] > 0:
+        bfs(i, j)
+        count += 1  # bfs로 찾은 빙하 덩어리 한 개
 
-    for i in range(n):
-        for k in range(m):
-            if visited[i][k] :
-                graph[i][k] -= (visited[i][k]-1)
-                if graph[i][k] < 0: graph[i][k] = 0
+  # 빙하 녹이기
+  for i in range(n):
+    for j in range(m):
+      if visited[i][j] != 0:
+        graph[i][j] -= (visited[i][j] - 1)
+        if graph[i][j] < 0:
+          graph[i][j] = 0
 
-    time += 1
-    if count == 0:
-        print(0)
-        exit()
-    if count >=2 :
-        break
+  # 빙하 덩어리 검사
+  if count >= 2:
+    print(time)
+    break
 
-print(time-1)
+  if count == 0:
+    print(0)
+    break
+
+  time += 1  # 1년 흐름
