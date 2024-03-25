98aadadcb6c4eff419e2dfbe248f888b856eeb54
Jimin Kim <74192997+kjimin0619@users.noreply.github.com>
2024-03-25 12:32:29 +0900

    [Gold III] Title: 다리 만들기, Time: 1908 ms, Memory: 34136 KB -BaekjoonHub

--- 백준/Gold/2146. 다리 만들기/다리 만들기.py
+++ 백준/Gold/2146. 다리 만들기/다리 만들기.py
@@ -1,67 +1,57 @@
-# 2146 다리 만들기
+import sys
 from collections import deque
 
-n = int(input().strip())
-graph = [list(map(int, input().split())) for _ in range(n)]
+ans = int(1e9)
 
+n = int(sys.stdin.readline().strip())
+graph = [list(map(int, sys.stdin.readline().split())) for _ in range(n)]
+inum = 2  # 대륙 번호
 direction = [(-1, 0), (1, 0), (0, 1), (0, -1)]
-inum = 2  # 육지번호
-ans = int(1e9)  # 최단거리
 
 
-# 대륙 번호 넘버링
-def island_numbering(i, j):
+def bfs(i, j):
   d = deque([(i, j)])
-
   while d:
     cx, cy = d.popleft()
-    graph[cx][cy] = inum
     for dx, dy in direction:
-      nx, ny = dx + cx, dy + cy
+      nx, ny = cx + dx, cy + dy
       if (0 <= nx < n and 0 <= ny < n and graph[nx][ny] == 1):
-        graph[nx][ny] = inum
         d.append((nx, ny))
+        graph[nx][ny] = inum
 
 
-# 다리 짓기
-def shortcut(inum):
-  # 각 대륙마다 최단거리 탐색
+def calPath(inum):
+  path = [[-1] * n for _ in range(n)]
   d = deque([])
-  dist = [[-1] * n for _ in range(n)]
-
   for i in range(n):
     for j in range(n):
       if graph[i][j] == inum:
-        dist[i][j] = 0
-        d.append((i, j))  # inum에 해당하는 육지들 전부 저장
+        path[i][j] = 0
+        d.append((i, j))
 
-  # 최단거리 탐색
   while d:
     cx, cy = d.popleft()
     for dx, dy in direction:
       nx, ny = cx + dx, cy + dy
       if (0 <= nx < n and 0 <= ny < n):
-        # 아직 방문 하지 않은 바다라면
-        if (graph[nx][ny] == 0 and dist[nx][ny] == -1):
-          dist[nx][ny] = dist[cx][cy] + 1
+        if graph[nx][ny] == 0 and path[nx][ny] == -1:  # 바다
+          path[nx][ny] = path[cx][cy] + 1
           d.append((nx, ny))
+        elif graph[nx][ny] and graph[nx][ny] != inum:
+          # 타대륙 도작
+          return path[cx][cy]
 
-        # 다른 대륙에 도착했다면
-        elif graph[nx][ny] > 0 and graph[nx][ny] != inum:
-          return dist[cx][cy]
-
-  return int(1e9)
+  return 1e9
 
 
 for i in range(n):
   for j in range(n):
     if graph[i][j] == 1:
-      island_numbering(i, j)
+      graph[i][j] = inum
+      bfs(i, j)
       inum += 1
 
-# 가장 짧은 다리 길이 찾기
 for num in range(2, inum):
-  temp = shortcut(num)
-  ans = min(ans, temp)
+  ans = min(ans, calPath(num))
 
 print(ans)
