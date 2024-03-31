fd0a9241fde86036fbc340beb54d72c6e42372a5
Jimin Kim <74192997+kjimin0619@users.noreply.github.com>
2024-03-31 16:50:03 +0900

    [Gold III] Title: 말이 되고픈 원숭이, Time: 5236 ms, Memory: 47908 KB -BaekjoonHub

--- 백준/Gold/1600. 말이 되고픈 원숭이/말이 되고픈 원숭이.py
+++ 백준/Gold/1600. 말이 되고픈 원숭이/말이 되고픈 원숭이.py
@@ -1,43 +1,42 @@
-# 1600 말이 되고픈 원숭이
+# 1600
 from collections import deque
 
 k = int(input().strip())
 w, h = map(int, input().split())
-horse_mv = [(-1, -2), (-2, -1), (-2, 1), (-1, 2), (1, -2), (2, -1), (1, 2),
-            (2, 1)]  # 말이 움직이는 방식
-monkey_mv = [(-1, 0), (1, 0), (0, 1), (0, -1)]  # 원숭이가 움직이는 방식
 
 board = [list(map(int, input().split())) for _ in range(h)]
-visited = [[[0] * w for _ in range(h)] for _ in range(k + 1)]  # 이동횟수 행, 열
-d = deque([(0, 0, 0)])
-flag = 0  # 도착여부
-visited[0][0][0] = 1  # 시작 지점
-while d:
-  cnt, ch, cw = d.popleft()
+visited = [[[0 for _ in range(w)] for _ in range(h)] for _ in range(k + 1)]
 
-  # 도착
-  if (ch == h - 1 and cw == w - 1):
-    print(visited[cnt][ch][cw] - 1)  # 시작지점 이동횟수 제외
+d = deque([])
+d.append((0, 0, 0))
+visited[0][0][0] = 1
+flag = 0
+
+while d:
+  tempK, x, y = d.popleft()
+
+  if y == h - 1 and x == w - 1:
+    print(visited[tempK][y][x] - 1)
     flag = 1
     break
 
-  # 원숭이처럼 이동
-  for i, j in monkey_mv:
-    nh, nw = ch + i, cw + j
-    if (0 <= nh < h and 0 <= nw < w and board[nh][nw] != 1
-        and not visited[cnt][nh][nw]):
-      visited[cnt][nh][nw] = visited[cnt][ch][cw] + 1
-      d.append((cnt, nh, nw))
+  # 원숭이 이동
+  for dx, dy in [(-1, 0), (1, 0), (0, 1), (0, -1)]:
+    nx, ny = dx + x, dy + y
+    if (0 <= nx < w and 0 <= ny < h and visited[tempK][ny][nx] == 0
+        and board[ny][nx] == 0):
+      visited[tempK][ny][nx] = visited[tempK][y][x] + 1
+      d.append((tempK, nx, ny))
 
   # 말처럼 이동
-  if cnt < k:
-    for i, j in horse_mv:
-      hh, hw = ch + i, cw + j
-      if (0 <= hh < h and 0 <= hw < w and board[hh][hw] != 1):
-        # 말처럼 이동
-        if not visited[cnt + 1][hh][hw]:
-          visited[cnt + 1][hh][hw] = visited[cnt][ch][cw] + 1
-          d.append((cnt + 1, hh, hw))
+  if tempK < k:
+    for dx, dy in [(-2, 1), (2, 1), (1, -2), (1, 2), (2, -1), (-2, -1),
+                   (-1, 2), (-1, -2)]:
+      nx, ny = dx + x, dy + y
+      if (0 <= nx < w and 0 <= ny < h and visited[tempK + 1][ny][nx] == 0
+          and board[ny][nx] == 0):
+        visited[tempK + 1][ny][nx] = visited[tempK][y][x] + 1
+        d.append((tempK + 1, nx, ny))
 
 if not flag:
   print(-1)
