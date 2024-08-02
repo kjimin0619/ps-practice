661d4148446c58c2dbcadf4dfa0fc489a66dbe25
Jimin Kim <74192997+kjimin0619@users.noreply.github.com>
2024-08-03 01:46:42 +0900

    [Bronze IV] Title: 알파벳 개수, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/10808. 알파벳 개수/알파벳 개수.cc
+++ 백준/Bronze/10808. 알파벳 개수/알파벳 개수.cc
@@ -5,16 +5,13 @@ using namespace std;
 int main() {
   string s;
   cin >> s;
-
   int count[26] = {0}; // 초기화 0
 
-  for (auto temp : s) {
+  for (auto temp : s)
     count[temp - 'a']++;
-  }
 
-  for (auto cnt : count) {
+  for (auto cnt : count)
     cout << cnt << ' ';
-  }
 
   return 0;
 }
